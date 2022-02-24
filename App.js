2b2d33ab861094c105b3c86a75bf5e695b056f19
Mars881 <75521799+Mars881@users.noreply.github.com>
2022-02-24 17:08:29 +0800

    myc

--- App.js
+++ App.js
@@ -3,17 +3,10 @@ import { SafeAreaView, View } from "react-native";
 import MainNavigation from "./src/Components/Navigation/MainNavigation";
 import "react-native-get-random-values";
 import { NavigationContainer, navigationRef } from "@react-navigation/native";
-<<<<<<< HEAD
-import { ApplicationProvider , IconRegistry} from '@ui-kitten/components';
-import * as eva from '@eva-design/eva';
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import { default as theme } from './src/theme/custom-theme.json';
-=======
 import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
 import * as eva from "@eva-design/eva";
 import { EvaIconsPack } from "@ui-kitten/eva-icons";
 import { default as theme } from "./src/theme/custom-theme.json";
->>>>>>> 83a5b9b2c31fbffab1b1782cc7337a11f950ad1a
 
 export default class App extends Component {
     constructor(props) {
@@ -25,14 +18,10 @@ export default class App extends Component {
             <>
             
                 <IconRegistry icons={EvaIconsPack} />
-<<<<<<< HEAD
-                <ApplicationProvider {...eva} theme={{...eva.light, ...theme}}>
-=======
                 <ApplicationProvider
                     {...eva}
                     theme={{ ...eva.light, ...theme }}
                 >
->>>>>>> 83a5b9b2c31fbffab1b1782cc7337a11f950ad1a
                     <NavigationContainer>
                         <MainNavigation />
                     </NavigationContainer>
