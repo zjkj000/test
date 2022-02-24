534c4826c2e85fc353ccb6aaad67f97d23583def
Mars881 <75521799+Mars881@users.noreply.github.com>
2022-02-24 11:29:07 +0800

    myc

--- App.js
+++ App.js
@@ -3,16 +3,10 @@ import { SafeAreaView, View } from "react-native";
 import MainNavigation from "./src/Components/Navigation/MainNavigation";
 import "react-native-get-random-values";
 import { NavigationContainer, navigationRef } from "@react-navigation/native";
-<<<<<<< HEAD
 import { ApplicationProvider , IconRegistry} from '@ui-kitten/components';
 import * as eva from '@eva-design/eva';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { default as theme } from './src/theme/custom-theme.json';
-=======
-import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
-import { EvaIconsPack } from "@ui-kitten/eva-icons";
-import * as eva from "@eva-design/eva";
->>>>>>> aeca67833dbf826b6a2ec125e9b148992229e461
 
 export default class App extends Component {
     constructor(props) {
@@ -22,12 +16,9 @@ export default class App extends Component {
     render() {
         return (
             <>
+            
                 <IconRegistry icons={EvaIconsPack} />
-<<<<<<< HEAD
                 <ApplicationProvider {...eva} theme={{...eva.light, ...theme}}>
-=======
-                <ApplicationProvider {...eva} theme={eva.light}>
->>>>>>> aeca67833dbf826b6a2ec125e9b148992229e461
                     <NavigationContainer>
                         <MainNavigation />
                     </NavigationContainer>
---
+++ src/pages/Wrongbook/Wrongbook.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { View , Image, Text ,StyleSheet} from "react-native";
+import { Card } from "@ui-kitten/components";
+
+export default Wrongbook = () =>{
+    //渲染
+    return(
+        <View>
+            <Card>
+
+            </Card>
+
+        </View>
+    )
+};
+const styles = StyleSheet.create({
+    View:{
+        flex:
+    }
+})
